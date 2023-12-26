cd3ded929e6dfea0920a44836e3cde1913ba8ec2
Isaiah Mann <isaiahbmann@gmail.com>
2023-12-26 10:24:19 -0800

    Add kind_cloud to projects

--- shell/copy-project-template.sh
+++ shell/copy-project-template.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# copies the projects/template/index.html file into all the project directories
+
 template_dir="projects/template/";
 project_dirs=($(find "projects" -type d))
 for dir in "${project_dirs[@]}"; do
